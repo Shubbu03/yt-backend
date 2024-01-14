5e92485a217cf52025dc12338ca2c90f8bf58383
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-01-14 19:02:42 +0530

    Add multer middleware and cloudinary utility

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
-  });
+  //MY ATTEMPT=>
+  //show ui to user
+  // take details
+  //send post request
+  //store in db
+
+  //PROPER CORRECT STEPS =>
+  //take user data
+  //validation - should not be empty
+  //check if already exist
+  //check for images ,avatar
+  //upload them to cloudnary,avatar
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation response
+  //return response
+
+  const { fullname, email, username, password } = req.body;
+
+  console.log("email:",email)
 });
 
 export { registerUser };
